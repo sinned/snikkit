abd2a348e113460fcd364a8c983ed87b79e08eba
sinned <sinned@gmail.com>
2011-06-03 01:12:58 -0700

    Initial commit. yay.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snikkit::Application.config.secret_token = '1a7c91541b4c37fcc4e8246d1ef837d80c0b1d34d24b9590f63475ed57235c07c9816b5935fad63c04c812ef23dff3a2dc53af8e15293661770f76a5dc8ec69f'
